5f689df30f1962163ee4bb197526d861aba17e98
mrava87 <matteoravasi@gmail.com>
2025-09-01 20:18:14 +0100

    minor: use pytest.skip instead of return in test_torchoperator

--- pytests/test_torchoperator.py
+++ pytests/test_torchoperator.py
@@ -32,8 +32,8 @@ def test_TorchOperator(par):
     """
     # temporarily, skip tests on mac as torch seems not to recognized
     # numpy when v2 is installed
-    if platform.system() == "Darwin":
-        return
+    if platform.system() == "darwin":
+        pytest.skip("torch not recognized on macOS with numpy v2")
     device = "cpu" if backend == "numpy" else "cuda"
 
     Dop = MatrixMult(np.random.normal(0.0, 1.0, (par["ny"], par["nx"])))
@@ -62,8 +62,8 @@ def test_TorchOperator_batch(par):
     """Apply forward for input with multiple samples (= batch) and flattened arrays"""
     # temporarily, skip tests on mac as torch seems not to recognized
     # numpy when v2 is installed
-    if platform.system() == "Darwin":
-        return
+    if platform.system() == "darwin":
+        pytest.skip("torch not recognized on macOS with numpy v2")
     device = "cpu" if backend == "numpy" else "cuda"
 
     Dop = MatrixMult(np.random.normal(0.0, 1.0, (par["ny"], par["nx"])))
@@ -84,8 +84,8 @@ def test_TorchOperator_batch_nd(par):
     """Apply forward for input with multiple samples (= batch) and nd-arrays"""
     # temporarily, skip tests on mac as torch seems not to recognized
     # numpy when v2 is installed
-    if platform.system() == "Darwin":
-        return
+    if platform.system() == "darwin":
+        pytest.skip("torch not recognized on macOS with numpy v2")
     device = "cpu" if backend == "numpy" else "cuda"
 
     Dop = MatrixMult(np.random.normal(0.0, 1.0, (par["ny"], par["nx"])), otherdims=(2,))
