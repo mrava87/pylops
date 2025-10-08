76876ee9374099fdac7b16d2d600334c99e38193
mrava87 <matteoravasi@gmail.com>
2025-10-08 16:25:58 +0100

    doc: started to add Attributes to signalprocessing

--- pylops/signalprocessing/convolve1d.py
+++ pylops/signalprocessing/convolve1d.py
@@ -238,11 +238,21 @@ class Convolve1D(LinearOperator):
 
     Attributes
     ----------
+    nh : :obj:`int`
+        Length of the filter
+    hstar : :obj:`numpy.ndarray`
+        Time-reversed filter used in adjoint
+    convfunc : :obj:`callable`
+        Function handler used to perform convolution
+    dims : :obj:`tuple`
+        Shape of the array after the adjoint, but before flattening.
+
+        For example, ``x_reshaped = (Op.H * y.ravel()).reshape(Op.dims)``.
+    dimsd : :obj:`tuple`
+        Shape of the array after the forward, but before flattening. In
+        this case, same as ``dims``.
     shape : :obj:`tuple`
         Operator shape
-    explicit : :obj:`bool`
-        Operator contains a matrix that can be solved
-        explicitly (``True``) or not (``False``)
 
     Raises
     ------
