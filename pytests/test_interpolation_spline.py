33f47c48fae86715e59537a7ad02e97bd5195665
MothNik <nik.zoe@web.de>
2025-12-30 10:33:32 +0100

    test: `test_interpolation_spline` - made casting to complex more explicit

--- pytests/test_interpolation_spline.py
+++ pytests/test_interpolation_spline.py
@@ -1,16 +1,19 @@
+from typing import Final, Tuple
+
 import numpy as np
 import pytest
 from scipy.interpolate import CubicSpline
 
 from pylops.signalprocessing import InterpCubicSpline
 
-TEST_ARRAY_SHAPE = (
+TEST_ARRAY_SHAPE: Final[Tuple] = (
     20,
     51,
     2,  # <- this dimension is exactly 2 because this triggers a special solver
     12,
 )
-TEST_X_RANGE = (-5.0, 5.0)
+TEST_X_RANGE: Final[Tuple[float, float]] = (-5.0, 5.0)
+MIN_NUM_TEST_SAMPLES: Final[int] = 1
 
 
 @pytest.mark.parametrize(
@@ -60,11 +63,17 @@ def test_natural_cubic_spline_against_scipy(
         stop=TEST_X_RANGE[1],
         num=num_samples,
     )
-    y_fit = np.random.randn(*TEST_ARRAY_SHAPE) + (
-        with_complex * 1.0j
-    ) * np.random.randn(*TEST_ARRAY_SHAPE)
+    y_fit = np.random.randn(*TEST_ARRAY_SHAPE)
+    if with_complex:
+        y_fit = y_fit.astype(np.complex128)
+        y_fit.imag = np.random.randn(*TEST_ARRAY_SHAPE)
 
-    x_eval_fractions = np.random.rand(round(num_samples * subsample_fraction))
+    x_eval_fractions = np.random.rand(
+        max(
+            round(num_samples * subsample_fraction),
+            MIN_NUM_TEST_SAMPLES,
+        )
+    )
     x_eval_for_pylops = (num_samples - 1) * x_eval_fractions
     x_eval_for_scipy = TEST_X_RANGE[0] + x_eval_fractions * (
         TEST_X_RANGE[1] - TEST_X_RANGE[0]
